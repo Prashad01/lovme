abd570ed4d49573c95415177dc72b77a2cdb56c3
Prashad01 <93466156+Prashad01@users.noreply.github.com>
2024-03-04 22:47:40 +0530

    all files are uploaded

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+
+document.getElementById("moveButton").addEventListener("click", function() {
+  // Disable the button after it's clicked
+  this.disabled = true;
+  
+  // Move the button randomly on the screen
+  moveButtonRandomly(this);
+});
+
+function moveButtonRandomly(button) {
+  var container = document.querySelector('.container');
+  var containerRect = container.getBoundingClientRect();
+  var buttonRect = button.getBoundingClientRect();
+  
+  var maxX = containerRect.width - buttonRect.width;
+  var maxY = containerRect.height - buttonRect.height;
+  
+  // Generate random coordinates
+  var randomX = Math.floor(Math.random() * maxX);
+  var randomY = Math.floor(Math.random() * maxY);
+  
+  // Set new position
+  button.style.position = 'absolute';
+  button.style.left = randomX + 'px';
+  button.style.top = randomY + 'px';
+  
+  // Repeat after a short delay
+  setTimeout(function() {
+    moveButtonRandomly(button);
+  }, 1000); // Change the delay as needed
+}
+document.getElementById("yesBtn").addEventListener("click", function() {
+  window.location.href = "yes.html";
+});
